41828ef614d41e85f9692872b09029cd3d0c21bf
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-27 06:07:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
 
- array = string.split(" ") # can't put
+ array = string.split(" ") # can't put each, use split to 
  array.collect do |word|
    word.reverse! 
  end 
