89a0bb5408c92eaedd74acf3ea6bbc1d1c446849
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-27 06:16:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -15,6 +15,6 @@ end
 
 
 # turn string to array, can't put each b/c doesn't reverse use split to separate each word 
-# need to collect the array just made then while I collect the array, I need to reverse each word in places.
-# word will reverse in place 
-# I had to research "string to array"
+# need to collect the array just made then while I collect the array
+# string_word need to be reverse in place 
+# I had to research "string to array". I kepts on using each but it was given me a name error of each so I had to research why and then read about using split instead. 
