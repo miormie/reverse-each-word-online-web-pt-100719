c1d22a3a7269db7295aeeb2f65e805952a42359d
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-27 06:12:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -16,4 +16,5 @@ end
 
 # turn string to array, can't put each b/c doesn't reverse use split to separate each word 
 # need to collect the array just made then while I collect the array, I need to reverse each word in places.
-after that 
+# word will reverse in place 
+# I had to research "string to array"
